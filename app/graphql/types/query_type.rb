debabb465a7deb081329b7b70ade695e8bcf0490
timweri <ng.duc.tahn@gmail.com>
2019-01-20 12:38:33 -0500

    Add single product query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -9,11 +9,25 @@ Types::QueryType = GraphQL::ObjectType.define do
     }
   end
 
+  field :product, Types::ProductType do
+    argument :id, types.ID
+    argument :title, types.String
+    resolve -> (obj, args, ctx) {
+      if args[:id]
+        Product.find(args[:id])
+      elsif args[:title]
+        Product.where("title = ?", args[:title]).first
+      else
+        Product.first!
+      end
+    }
+  end
+
   field :allProducts, types[Types::ProductType] do
     argument :onlyShowAvailable, types.Boolean, default_value: false
     resolve -> (obj, args, ctx) {
       if args[:onlyShowAvailable]
-        Product.where('inventory_count > 0')
+        Product.where('inventory_count > ?', 0)
       else
         Product.all
       end
