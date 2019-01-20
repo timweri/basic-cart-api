c564c259480305a7eebb6d13db34ef3996c5b2a1
timweri <ng.duc.tahn@gmail.com>
2019-01-20 12:04:07 -0500

    Misc dev files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_20_055324) do
+ActiveRecord::Schema.define(version: 2019_01_20_150005) do
+
+  create_table "carts", force: :cascade do |t|
+    t.integer "products_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["products_id"], name: "index_carts_on_products_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
