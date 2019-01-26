cbf19c20a8df919994b5627d6e097239ab46eb71
timweri <ng.duc.tahn@gmail.com>
2019-01-26 12:02:37 -0500

    Enable GraphiQL for test environment

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   post '/query' => 'application#query'
-  if Rails.env.development?
+  if (Rails.env.development? || Rails.env.test?)
     mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/query"
   end
 end
